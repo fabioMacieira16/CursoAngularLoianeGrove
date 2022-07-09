8d7d475939f641e6b7e3862b25bf6af7f3048bff
Fabio Macieira <fabiomacieira16@gmail.com>
2022-07-09 17:51:44 -0300

    refactory : Modify campovalorInput

--- data-binding/src/app/output-property/output-property.component.ts
+++ data-binding/src/app/output-property/output-property.component.ts
@@ -14,12 +14,12 @@ export class OutputPropertyComponent implements OnInit {
   @ViewChild('campoInput') campoValorInput!: ElementRef;
 
   incrementa() {
-    this.valor = this.campoValorInput.nativeElement.value++;
+    this.campoValorInput.nativeElement.value++;
     this.mudouValor.emit({ novoValor: this.valor });
   }
 
   decrementa() {
-    this.valor = this.campoValorInput.nativeElement.value--;
+    this.campoValorInput.nativeElement.value--;
     this.mudouValor.emit({ novoValor: this.valor });
   }
 
